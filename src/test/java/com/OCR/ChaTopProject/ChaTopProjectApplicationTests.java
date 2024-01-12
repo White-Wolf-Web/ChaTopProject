778e2edbe1c3bae1622204363dda88ef0cc22bf1
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-01-12 14:30:49 +0100

    Initial commit

---
+++ src/test/java/com/OCR/ChaTopProject/ChaTopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OCR.ChaTopProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
