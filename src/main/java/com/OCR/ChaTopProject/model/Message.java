b3357c3a0ce7a0ffa6f6bfee1a731080fa736aca
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-01-19 19:02:27 +0100

    Security + connection avec Base SQL
    Configuration Security selon le cours donc à corriger !   La BDD a été créé puis  connecté après avoir du changé les identifiants

--- src/main/java/com/OCR/ChaTopProject/model/Message.java
+++ src/main/java/com/OCR/ChaTopProject/model/Message.java
@@ -2,10 +2,12 @@ package com.OCR.ChaTopProject.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Message {
-
+    @Id
+    private Long id;
 }
--- src/main/java/com/OCR/ChaTopProject/model/Rental.java
+++ src/main/java/com/OCR/ChaTopProject/model/Rental.java
@@ -1,10 +1,27 @@
 package com.OCR.ChaTopProject.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
-@Entity
+import java.util.Date;
+
 @Data
+@Table(name = "rentals")
+@Entity
+@ToString
+
 
 public class Rental {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private int surface;
+    private int price;
+    private String picture;
+    private String description;
+    private Long owner_id;
+    private Date created_at;
+    private Date updated_at;
 }
