2b34676a33386c1f1e2774182336f3180ee4ed2c
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-01-21 17:27:54 +0100

    Repository 99%

--- src/main/java/com/OCR/ChaTopProject/repository/MessageRepository.java
+++ src/main/java/com/OCR/ChaTopProject/repository/MessageRepository.java
@@ -2,6 +2,14 @@ package com.OCR.ChaTopProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.OCR.ChaTopProject.model.Message;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 }
+
+/*
+ * JpaRepository<T, ID> prend deux paramètres génériques : T est le type de l'entité (importé du model) et ID est le type de la clé primaire de l'entité.
+ * T est Message et ID est Long (puisque l'ID de l'utilisateur est de type Long
+ * Spring Data JPA implémente automatiquement les méthodes courantes de CRUD
+ * */
--- src/main/java/com/OCR/ChaTopProject/repository/UserRepository.java
+++ src/main/java/com/OCR/ChaTopProject/repository/UserRepository.java
@@ -2,6 +2,15 @@ package com.OCR.ChaTopProject.repository;
 
 import com.OCR.ChaTopProject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
+
+
+/*
+* JpaRepository<T, ID> prend deux paramètres génériques : T est le type de l'entité (importé du model) et ID est le type de la clé primaire de l'entité.
+* T est User et ID est Long (puisque l'ID de l'utilisateur est de type Long
+* Spring Data JPA implémente automatiquement les méthodes courantes de CRUD
+* */
